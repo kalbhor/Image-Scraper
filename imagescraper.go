593bcf1c4dddc1ddb2e1b64fe1d5ed9f3e72d4e2
Alex <alex@mindscape.co.nz>
2017-05-08 17:23:27 +1200

    more clean up

--- imagescraper.go
+++ imagescraper.go
@@ -14,7 +14,7 @@ import (
 var inserts sync.WaitGroup
 var downloads sync.WaitGroup
 
-func Crawl(url string, ch chan string) {
+func CrawlPage(url string, ch chan string) {
 	fmt.Printf("\n\nFetching Page..\n")
     resp, err := goquery.NewDocument(url)
 
@@ -37,10 +37,36 @@ func Crawl(url string, ch chan string) {
     inserts.Done()
 }
 
+func CrawlPages(urls []string, imageUrls chan string){
+    // Crawl process (concurrently)
+    for _, url := range urls {
+        if url[:4] != "http"{
+            url = "http://" + url
+        }
+        inserts.Add(1)
+        go CrawlPage(url, imageUrls)
+    }
+}
 
-func DownloadImg(url string, folder string, sem chan bool) {
+// takes a channel of incoming URLs and outputs a channel of unique URLs
+func EnsureUnique(in chan string, out chan string) {   
+    allUrls := make(map[string]bool)
+
+    go func() {
+        for url := range in {
+            if !allUrls[url] {
+                allUrls[url] = true
+                fmt.Printf("Enqueuing %s \n", url)
+                out <- url
+            }
+        }
+        // once in closes and the last url is pushed onto the out
+        close(out)
+    }()
+}
+
+func DownloadImage(url string, folder string, sem chan bool) {
 	os.Mkdir(folder, os.FileMode(0777))
-
     defer downloads.Done()
 
     if url[:4] != "http"{
@@ -61,24 +87,10 @@ func DownloadImg(url string, folder string, sem chan bool) {
     }
 	io.Copy(file, resp.Body)
 	resp.Body.Close()
+
 	fmt.Printf("Saving %s \n", folder + "/" + name)
 
     <- sem
-}
-
-func EnsureUnique(in chan string, out chan string) {   
-    allUrls := make(map[string]bool)
-
-    go func() {
-        for thing := range in {
-            if !allUrls[thing] {
-                allUrls[thing] = true
-                fmt.Printf("Enqueuing %s \n", thing)
-                out <- thing
-            }
-        }
-        close(out)
-    }()
 }
 
 func DownloadImages(in chan string, Folder string) {
@@ -90,20 +102,9 @@ func DownloadImages(in chan string, Folder string) {
         for ui := range in {
             sem <- true
             downloads.Add(1)
-            go DownloadImg(ui, Folder, sem)
+            go DownloadImage(ui, Folder, sem)
         }
     }()
-}
-
-func CrawlSites(urls []string, imageUrls chan string){
-    // Crawl process (concurrently)
-    for _, url := range urls {
-        if url[:4] != "http"{
-            url = "http://" + url
-        }
-        inserts.Add(1)
-        go Crawl(url, imageUrls)
-    }
 }
 
 func main() {
@@ -115,16 +116,19 @@ func main() {
     Folder := os.Args[1]
     seedUrls := os.Args[2:]
 
-    // Channels
     imageUrls := make(chan string)
     uniqueImgUrls := make(chan string)
 
-    CrawlSites(seedUrls, imageUrls)
+    // Crawl websites and push image urls onto the imageUrls channel
+    CrawlPages(seedUrls, imageUrls)
 
+    // Ingest urls from imageUrls channel and output unique images onto uniqueImageUrls channel
     EnsureUnique(imageUrls, uniqueImgUrls)
 
+    // Ingest urls from uniqueImageUrls channel, download and write into Folder
     DownloadImages(uniqueImgUrls, Folder)
 
+    // inserts waitgroup is incremented by the Crawl
     inserts.Wait()
     close(imageUrls)
     
